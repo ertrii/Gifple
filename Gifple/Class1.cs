aced1752d4e142ac15654c81221f2c17b7cd649d
ertrii <ertriilive@gmail.com>
2018-09-01 20:14:01 -0500

    init

--- Gifple/Class1.cs
+++ Gifple/Class1.cs
@@ -6,7 +6,73 @@ using System.Threading.Tasks;
 
 namespace Gifple
 {
-    public class Class1
+    public class Gifple
     {
+        private String name;
+        private string description = null;
+        private IMGFileGifple[] imageFiles;
+
+        public Gifple(String name)
+        {
+            this.name = name;
+        }
+
+        public IMGFileGifple[] ImageFiles
+        {
+            set
+            {
+                imageFiles = value;
+            }
+
+        }
+        public bool Repeat = true;
+        public string Autor = "Anonymous";
+
+
+        public String Description
+        {
+            set
+            {
+                description = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Generate sequence images.png in HTML file.
+        /// </summary>
+        /// <returns></returns>
+        public bool Generate(String outputPath)
+        {
+            if (imageFiles.Length < 1) return false;
+            return true;
+        }
+
+    }
+
+    public class IMGFileGifple
+    {
+        public String Path;
+        private int delay = 60;
+        public int X;
+        public int Y;
+
+        public int Delay
+        {
+            set
+            {
+                if (value >= 0) delay = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+            get
+            {
+                return delay;
+            }
+        }
+        public void Vector(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 }
